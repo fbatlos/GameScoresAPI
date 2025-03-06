ab50695f5bb9ee7dc63157a4cd179bae337da5a3
fbatlos <bdlsfrancisco@iesrafaelalberti.es>
2025-03-06 16:51:54 +0100

    Cambio en la bd

--- GameScoresAPI/obj/Debug/net8.0/GameScoresAPI.AssemblyInfo.cs
+++ GameScoresAPI/obj/Debug/net8.0/GameScoresAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameScoresAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a5f0128ad19ef3fa085affc4794b6378b5fb7a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3864819ff6cfc97b82ec13539594ca2300b63b5c")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameScoresAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameScoresAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
