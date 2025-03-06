d637cfa12081641d63206706e4a72fa2620539a8
fbatlos <bdlsfrancisco@iesrafaelalberti.es>
2025-03-06 16:59:20 +0100

    Ahora la bd debería existir

--- GameScoresAPI/obj/Debug/net8.0/GameScoresAPI.AssemblyInfo.cs
+++ GameScoresAPI/obj/Debug/net8.0/GameScoresAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameScoresAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3864819ff6cfc97b82ec13539594ca2300b63b5c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5816ac6beab82097a4cc09bf0f3c0332d838e99")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameScoresAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameScoresAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
